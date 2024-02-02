5395048663011ffc399c5f011723ed2f511e5742
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-02 20:42:28 -0300

    chore(jest): config scripts to run unit and integrations

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/test'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/test/**', '!**config/**'],
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1'
+  }
+}
