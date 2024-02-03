abe7183616aeacfd3d68a65019c50dd87f550727
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-02 21:00:50 -0300

    refactor: move app express to another file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import express from "express";
+import app from './app'
 
-const app = express();
-
-const port = process.env.PORT || 4568;
-
-app.get("/ping", (req, res) => {
-  return res.send("pong");
-});
+const port = process.env.PORT ?? 4568
 
 app.listen(port, () => {
-  console.log(`Escutando na porta ${port}`);
-});
+  console.log(`Escutando na porta ${port}`)
+})
