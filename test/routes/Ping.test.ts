6913a1260b79e3cf5bf36a536567c63eb00d1bc5
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-05 14:19:48 -0300

    fix: get app from test application

--- test/routes/Ping.test.ts
+++ test/routes/Ping.test.ts
@@ -1,10 +1,16 @@
 import request from 'supertest'
 
-import app from '../../src/app'
+import Application from '../application'
 
 describe('Ping route', () => {
+  const application = new Application()
+
+  beforeAll(async () => {
+    await application.setup()
+  })
+
   test('should return status code 200 on success', async () => {
-    const response = await request(app).get('/ping').expect(200)
+    const response = await request(application.app).get('/ping').expect(200)
 
     expect(response.text).toBe('pong')
   })
