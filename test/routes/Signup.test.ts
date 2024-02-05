b3163e4cde28486bebb53e2e233f98536bddbad3
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-05 14:20:09 -0300

    test(signup): register an user

---
+++ test/routes/Signup.test.ts
@@ -0,0 +1,53 @@
+import { faker } from '@faker-js/faker'
+import request from 'supertest'
+
+import Application from '../application'
+
+describe('Signup route', () => {
+  const application = new Application()
+
+  beforeAll(async () => {
+    await application.setup()
+  })
+
+  test('should return an account on success', async () => {
+    const body = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: 'fake'
+    }
+    const response = await request(application.app)
+      .post('/api/v1/signup')
+      .send(body)
+      .expect(200)
+
+    expect(JSON.parse(response.text)).toEqual({
+      user: {
+        id: expect.any(String),
+        name: body.name,
+        email: body.email,
+        password: expect.any(String)
+      },
+      accessToken: expect.any(String)
+    })
+  })
+
+  test('should throw error if alread exists an account with same e-mail', async () => {
+    const body = {
+      name: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: 'fake'
+    }
+
+    await request(application.app).post('/api/v1/signup').send(body).expect(200)
+
+    const response = await request(application.app)
+      .post('/api/v1/signup')
+      .send(body)
+      .expect(403)
+
+    expect(JSON.parse(response.text)).toEqual({
+      errors: 'Email already exists'
+    })
+  })
+})
