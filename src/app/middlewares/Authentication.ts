57f31ed59d3349edef09594c57e13f120184868d
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-05 11:32:39 -0300

    feat(middleware): create and decode accessToken

---
+++ src/app/middlewares/Authentication.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/non-nullable-type-assertion-style */
+import jwt from 'jsonwebtoken'
+import NotAuthorized from 'src/errors/NotAuthorized'
+
+export default class Authentication {
+  decodeAccessToken(accessToken: string | undefined): string {
+    if (!accessToken) {
+      throw new NotAuthorized('No token provided')
+    }
+
+    try {
+      const userId = jwt.verify(accessToken, process.env.JWT_SECRET as string)
+      return userId as string
+    } catch {
+      throw new NotAuthorized('Invalid token')
+    }
+  }
+
+  createAccessToken(userID: string): string {
+    const accessToken = jwt.sign(userID, process.env.JWT_SECRET as string)
+    return accessToken
+  }
+}
