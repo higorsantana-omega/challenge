63fc429eccb23a8682e1ed41390702dd4d30b75a
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-05 11:33:57 -0300

    feat: add base class to use in controllers

---
+++ src/app/controllers/BaseController.ts
@@ -0,0 +1,23 @@
+import { type Request, type Response } from 'express'
+import { type Interactors } from 'src/interactors'
+
+import HandleError from '../middlewares/HandleError'
+
+export abstract class BaseController {
+  constructor(protected interactors: Interactors) {}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      return await this.execute(request, response)
+    } catch (error) {
+      return HandleError.execute(error as Error, response)
+    }
+  }
+
+  protected abstract execute(
+    request: Request,
+    response: Response
+  ): Promise<Response>
+}
+
+export type { Request, Response }
