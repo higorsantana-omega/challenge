30da545703a8e907eead952681bddc3547ec554c
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-05 14:19:19 -0300

    fix: return in correct type

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { type PrismaClient, type UserSchema } from '@prisma/client'
 
 import { type Repository } from './Repository'
@@ -5,10 +6,10 @@ import { type Repository } from './Repository'
 export class UserRepository implements Repository {
   constructor(private readonly prismaClient: PrismaClient) {}
 
-  async findById(id: string) {
-    return await this.prismaClient.userSchema.findFirst({
+  async findById<T>(id: string): Promise<T> {
+    return (await this.prismaClient.userSchema.findFirst({
       where: { id }
-    })
+    })) as unknown as T
   }
 
   async findByEmail(email: string) {
@@ -17,9 +18,9 @@ export class UserRepository implements Repository {
     })
   }
 
-  async save<T>(entity: T) {
-    return await this.prismaClient.userSchema.create({
+  async save<T>(entity: T): Promise<T> {
+    return (await this.prismaClient.userSchema.create({
       data: entity as UserSchema
-    })
+    })) as unknown as T
   }
 }
