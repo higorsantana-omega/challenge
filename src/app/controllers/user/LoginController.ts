abecafb648080bbcd57bcfa69c2015a0a2eb8cf1
higorsantana-omega <higor.santanahs17@gmail.com>
2024-02-05 16:22:38 -0300

    feat(controllers): add expected request

--- src/app/controllers/user/LoginController.ts
+++ src/app/controllers/user/LoginController.ts
@@ -1,7 +1,14 @@
+import { z } from 'zod'
+
 import { AccessController } from './AccessController'
 import { type Request } from '../BaseController'
 
 export class LoginController extends AccessController {
+  protected expectedRequest = z.object({
+    email: z.string(),
+    password: z.string()
+  })
+
   protected async setupAccount(request: Request): Promise<void> {
     this.user = await this.interactors.user.login({
       email: request.body.email,
--- src/app/controllers/user/SignupController.ts
+++ src/app/controllers/user/SignupController.ts
@@ -1,7 +1,15 @@
+import { z } from 'zod'
+
 import { AccessController } from './AccessController'
 import { type Request } from '../BaseController'
 
 export class SignupController extends AccessController {
+  protected expectedRequest = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string()
+  })
+
   protected async setupAccount(request: Request): Promise<void> {
     this.user = await this.interactors.user.signup({
       email: request.body.email,
